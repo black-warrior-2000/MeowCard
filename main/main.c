7d14a7f79a9d8256c7f8ec32403949b8e64e7cb2
BlockYang <2281842761@qq.com>
2024-06-19 01:34:09 +0800

    light up SPI LCD & show string

--- main/main.c
+++ main/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include "esp_system.h"
+#include "nvs_flash.h"
 
+/*freertos*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+
+/*bsp driver*/
+#include "lcd.h"
+
+
+//the main function
 void app_main(void)
 {
+    /*esp system init*/
+    esp_err_t ret;
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
 
+    /*lcd init*/
+    spi2_init();
+    lcd_init();
+    while(1){
+        lcd_show_string(10, 40, 240, 32, 32, "MeowCard", RED);
+        vTaskDelay(500);
+    }
 }
